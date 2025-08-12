627427e903fe4bf62488f866e5103017a0c36119
Fikayo <Fikayoz@gmail.com>
2025-08-12 18:43:07 +0900

    Added question objects to javascript logic file

--- script.js
+++ script.js
@@ -7,5 +7,32 @@ const questions = [
             {text: "Elephant", correct: false},
             {text: "Giraffe", correct: false}
         ]
-    }
+    },
+    {
+        question: "What is the smallest city in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false}
+        ]
+    },
+    {
+        question: "What is the largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true}
+        ]
+    },
+    {
+        question: "What is the smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false}
+        ]
+    },
 ];
