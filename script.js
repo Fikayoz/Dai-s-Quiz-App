1144a701e52bd066603a03ad3cf63079c7d76c97
Fikayo <Fikayoz@gmail.com>
2025-08-15 21:06:54 +0900

    Added logic functions to script and correct/incorrect answer css styling

--- script.js
+++ script.js
@@ -37,16 +37,65 @@ const questions = [
     },
 ];
 
+// Declare html elements that will be affected by script
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
+// Tracker for what question we're on and how many have been correctly answered
 let currentQuestionIndex = 0;
 let score = 0;
 
+// Function to begin the quiz
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+
+
+function showQuestion(){
+    // Resets html state of questions to prevent stacking questions on page
+    resetState();
+
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question; 
+
+    // For each possible answer per question, create an HTML button element with the answer's text within it
+    // and add it to the page
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+
+        // dataset refers to an html elements custom attributes. Updates the "correct" attribute of the button
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+}
+
+// Clears previous questions before moving onto next set
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    };
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
